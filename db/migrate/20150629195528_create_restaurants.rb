4dc964bfffc4be909358ade178abcc8ae7491fad
Drew Kirstein <kirstein.andrew@gmail.com>
2015-06-29 16:07:08 -0400

    add restaurants db & migration

---
+++ db/migrate/20150629195528_create_restaurants.rb
@@ -0,0 +1,12 @@
+class CreateRestaurants < ActiveRecord::Migration
+  def change
+    create_table :restaurants do |t|
+      t.string :name, null: false
+      t.string :address, null: false
+      t.string :city, null: false
+      t.string :state, null: false
+      t.integer :zip_code
+    end
+    add_index :restaurants, :name, unique: true
+  end
+end
