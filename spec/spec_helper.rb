7333727a91265705e4e06c6248c9a9cb8b4c893b
Jesse Norris <jesserae.norris@gmail.com>
2015-07-02 15:13:56 -0400

    Add delete restaurant with user authentification

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,7 @@ require 'coveralls'
 Coveralls.wear!('rails')
 
 def sign_in_as(user)
+  visit root_path
   click_link "Sign In"
   fill_in 'Email', with: user.email
   fill_in 'Password', with: user.password
