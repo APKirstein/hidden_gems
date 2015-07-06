6a2c79099528325d743100e1d8a75b2a4feab1e1
rovaira <rovaira@gmail.com>
2015-07-06 10:39:51 -0400

    WIP admin user story #1

---
+++ spec/features/admin/admin_views_users_list_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature 'admin views list of users', %{
+  As an administrator of Date Bites
+  I want to see a list 
+  So that I can post items and review them
+} do
+
+  # Acceptance Criteria:
+  # [x] I must specify a valid email address, username,
+  #   password, and password confirmation
+  # [x] If I don't specify the required information, I am presented with
+  #   an error message
+
+end
