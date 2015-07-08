a20aeb30d4b26526d0f84942fa795a8580ed6dc7
rovaira <rovaira@gmail.com>
2015-07-08 12:12:07 -0400

    fix factory_girls_rails in Gemfile, spec_helper and seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-require 'factory_girl'
+require 'factory_girl_rails'
 require './spec/support/factory_girl'
 
 # This file should contain all the record creation needed to seed the database with its default values.
