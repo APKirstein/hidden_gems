d098cf7da2fb1c1866a3ddb42bcdee3297f2810f
rovaira <rovaira@gmail.com>
2015-07-08 14:54:05 -0400

    Add admin ability to delete reviews

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :users, only: [:index, :destroy]
     resources :restaurants, only: [:index, :destroy]
+    resources :reviews, only: [:index, :destroy]
   end
 
   resources :restaurants do
