e7e3a2aad377bb10cc631bf9d0f65e285bbeb8b1
Jesse Norris <jesserae.norris@gmail.com>
2015-07-09 14:06:45 -0400

    Add styling

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -8,4 +8,40 @@ class Restaurant < ActiveRecord::Base
   validates :city, presence: true
   validates :state, presence: true, inclusion: { in: STATE }
   validates :zip_code, format: { with: /\d{5}/ }, allow_blank: true
+
+  def average_ranking(column)
+    if !Review.where(restaurant_id: id).empty?
+      average_score = Review.where(restaurant_id: id).average(column.to_sym)
+      average_score.round
+    else
+      "n/a"
+    end
+  end
 end
+
+
+
+
+# Grouped values: This returns an ordered hash of the values and groups them. It
+#   takes either a column name, or the name of a belongs_to association.
+#
+#     values = Person.group('last_name').maximum(:age)
+#     puts values["Drake"]
+#     # => 43
+#
+#     drake  = Family.find_by(last_name: 'Drake')
+#     values = Person.group(:family).maximum(:age) # Person belongs_to :family
+#     puts values[drake]
+#     # => 43
+#
+#     values.each do |family, max_age|
+#     ...
+#     end
+#
+# Person.calculate(:count, :all) # The same as Person.count
+# Person.average(:age) # SELECT AVG(age) FROM people...
+#
+# # Selects the minimum age for any family without any minors
+# Person.group(:last_name).having("min(age) > 17").minimum(:age)
+#
+# Person.sum("2 * age")
