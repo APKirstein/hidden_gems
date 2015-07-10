f3550daadee82eecb0758ad439ab25ef08d0bbe2
Jesse Norris <jesserae.norris@gmail.com>
2015-07-10 09:30:19 -0400

    addressed comments

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,6 @@
 class ReviewsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
-
+  before_action :authenticate_user!
+  
   def new
     @review = Review.new
   end
